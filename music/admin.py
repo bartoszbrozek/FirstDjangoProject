e504efb4fc03d9a07a6e72fcaa8769fb26aa792c
Barteł <bartekbrozek@gmail.com>
2017-03-02 20:13:31 +0100

    24

--- music/admin.py
+++ music/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Album
+from .models import Album, Song
 # admin pass1234
 
 admin.site.register(Album)
+admin.site.register(Song)
--- music/views.py
+++ music/views.py
@@ -1,16 +1,31 @@
-from django.http import HttpResponse
-from django.template import loader
-from .models import Album
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
+
     context = {
         'all_albums': all_albums,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
-    return HttpResponse("<h3>Details for Album id: " + album_id + "</h3>")
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(id=request.POST['song'])
+    except (KeyError, Exception):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "YOu DUMPY DUMB. IT IS NOT A VALID SONG ID... whatever."
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
